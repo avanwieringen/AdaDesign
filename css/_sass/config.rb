22067e5c642b7d56cbbeef3d26900a5f8537fe9c
Arjan van Wieringen <arjan.van.wieringen+git@gmail.com>
2012-01-29 16:38:46 +0100

    Latest updates

--- css/_sass/config.rb
+++ css/_sass/config.rb
@@ -7,6 +7,8 @@ sass_dir = "./"
 images_dir = "../../img/"
 javascripts_dir = "../../js/"
 
+output_style = :compressed
+
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
 
